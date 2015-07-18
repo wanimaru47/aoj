ef21930f804132e22e948faadf49a477d43d6cc4
wanimaru47 <wanimaru47@gmail.com>
2015-07-18 19:59:59 +0900

    [status] AC

--- volume21/aoj2165.cpp
+++ volume21/aoj2165.cpp
@@ -1,19 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main ()
-{
+const double INF = (double)(1 << 28);
+const int MOD = 256;
+const double EPS = 1e-10;
+
+int main () {
     int n;
-    cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
+    while (cin >> n, n) {
+        vector<int> I(n);
+        for (auto &i : I) cin >> i; 
+
+        double H = INF;
+        int S, A, C;
+        for (int s = 0; s < 16; s++) {
+            for (int a = 0; a < 16; a++) {
+                for (int c = 0; c < 16; c++) {
+                    vector<int> o(MOD, 0);
+                    int r = s;
+                    for (int i = 0; i < n; i++) {
+                        r = (r * a + c) % MOD;
+                        o[(r + I[i]) % MOD]++;
+                    }
+                    double h = 0.;
+                    for (auto i : o) {
+                        if (i > 0) h -= ((double)i / n) * log((double)i / n);
+                    }
+                    if (h + EPS < H) {
+                        H = h, S = s, A = a, C = c;
+                    }
+                }
+            }
+        }
+        cout << H << endl;
+        cout << S << " " << A << " " << C << endl;
     }
 
-    int m = 256;
-
-    int r = s;
-    
-        
     return 0;
 }
