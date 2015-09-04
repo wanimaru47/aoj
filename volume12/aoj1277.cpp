4b530f08ed63ee31344f992a9ffd04c23c1fba5a
wanimaru47 <wanimaru47@gmail.com>
2015-09-04 17:47:53 +0900

    writing

---
+++ volume12/aoj1277.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> V;
+
+int main () {
+    int N, T, L, B;
+    cin (cin >> N >> T >> L >> B, N || T || L || B) {
+        V lose(L), back(B);
+
+        for (auto &i : lose) cin >> i;
+        for (auto &i : back) cin >> i;
+
+        
+    }
+
+    return 0;
+}
