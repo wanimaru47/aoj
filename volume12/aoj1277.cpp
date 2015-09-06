81425bf6cfbe7f642425efc84fdaed35067791fc
wanimaru47 <wanimaru47@gmail.com>
2015-09-06 22:19:19 +0900

    akita

--- volume12/aoj1277.cpp
+++ volume12/aoj1277.cpp
@@ -11,7 +11,22 @@ int main () {
         for (auto &i : lose) cin >> i;
         for (auto &i : back) cin >> i;
 
+        vector<V> dp(T+1, V(L+1));
+        dp[0][0] = 1;
         
+        for (int i = 0; i < T; i++) {
+            for (int j = 0; j <= L; j++) {
+                if (dp[i][j] > 0) {
+                    if (find(lose.beign(), lose.end(), j) != lose.end()) {
+                        
+                    } else if (find(back.begin(), back.end(), j) != back.end()) {
+                        dp[i+1][0] += dp[i][j];
+                    } else {
+
+                    }
+                }
+            }
+        }
     }
 
     return 0;
