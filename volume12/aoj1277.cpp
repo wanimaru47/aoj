afe370d2f936bc1647830280b18f440415e0b005
wanimaru47 <wanimaru47@gmail.com>
2015-09-11 14:37:29 +0900

    AC

--- volume12/aoj1277.cpp
+++ volume12/aoj1277.cpp
@@ -1,45 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef vector<int> V;
+typedef vector<double> V;
 
 int main () {
     int N, T, L, B;
     while (cin >> N >> T >> L >> B, N || T || L || B) {
         V lose(L,0), back(B,0);
-        auto sum = pow(6.0,(double)T);
 
         for (int i = 0; i < L; i++) cin >> lose[i];
         for (int i = 0; i < B; i++) cin >> back[i];
 
         vector<V> dp(T+3, V(N+1));
-        dp[0][0] = 1;
+        dp[0][0] = 1.L;
         
         for (int i = 0; i < T; i++) {
-            for (int j = 0; j <= N; j++) {
+            dp[i+1][N] += dp[i][N];
+            for (int j = 0; j < N; j++) {
                 if (dp[i][j] > 0) {
                     for (int k = 1; k < 7; k++) {
                         int next = j + k <= N ? j + k : N * 2 - (j + k);
                         if (find(lose.begin(), lose.end(), next) != lose.end()) {
-                            dp[i+2][next] += dp[i][j];
+                            dp[i+2][next] += dp[i][j] / 6;
                         } else if (find(back.begin(), back.end(), next) != back.end()) {
-                            dp[i+1][0] += dp[i][j];
+                            dp[i+1][0] += dp[i][j] / 6;
                         } else {
-                            dp[i+1][next] += dp[i][j];
+                            dp[i+1][next] += dp[i][j] / 6;
                         }
                     }
                 }
             }
         }
 
-        for (int i = 0; i <= T; i++) {
-            for (int j = 0; j <= N; j++) {
-                cout << dp[i][j] << " ";
-            } cout << endl;
-        }
-
-        cout << "# " << sum << endl;
-        cout << ((double)dp[T][N] / sum) << endl;
+        printf("%.6f\n", dp[T][N]);
     }
 
     return 0;
