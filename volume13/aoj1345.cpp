673d902a23fe5a09ade714a0e360b69aee15f19f
wanimaru47 <wanimaru47@gmail.com>
2015-09-26 14:33:06 +0900

    writing

---
+++ volume13/aoj1345.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> s(n), t(m);
+    for (auto &i : s) cin >> i;
+    for (auto &i : t) cin >> i;
+
+    vector<int> r0, r1;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < t[i]; j++) {
+            r0.push_bach()
+                }
+    }
+
+    return 0;
+}
