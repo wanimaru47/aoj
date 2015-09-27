d9d70dea6a1f7e4da175f5479980a1d153809755
wanimaru47 <wanimaru47@gmail.com>
2015-09-27 15:51:53 +0900

    writting

---
+++ volume21/aoj2170.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vec;
+
+int main () {
+    int N, Q;
+    while (cin >> N >> Q, N || Q) {
+        vec p(N-1);
+        for (auto &i : vec) cin >> i;
+
+        char c;
+        int node;
+        
+        for (int i = 0; i < Q; i++) {
+            cin >> c >> node;
+
+            if (c == 'M') {
+
+            } else {
+
+            }
+        }
+    }
+    
+    return 0;
+}
