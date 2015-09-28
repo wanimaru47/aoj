4fbf2d1afd1107c36584abf4eed172d8647f06a3
wanimaru47 <wanimaru47@gmail.com>
2015-09-28 16:33:52 +0900

    w Input

---
+++ volume13/aoj1347.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vec;
+
+int main () { 
+    int N, m; cin >> N >> m;
+    vec c(m), d(m);
+    for (int i = 0; i < m; i++) {
+        cin >> c[i] >> d[i];
+    }
+    
+    return 0;
+}
