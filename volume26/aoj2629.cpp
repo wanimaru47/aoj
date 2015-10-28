5cbbc683558a3c16e9757cc85341e773c4e946d8
wanimaru47 <wanimaru47@gmail.com>
2015-10-28 21:07:06 +0900

    init

---
+++ volume26/aoj2629.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double d;
+    cin >> d;
+
+    if((int)d == d) {
+        printf("%.9f\n", d + 1.);
+    } else {
+        double l = 0., r = d * d;
+        while() {
+            double mid = (l + r) / 2.;
+        }
+    }
+
+    return 0;
+}
