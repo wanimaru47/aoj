63e8c8042795fb32772f617cb792700232f6490c
wanimaru47 <wanimaru47@gmail.com>
2015-11-12 19:57:00 +0900

    save

---
+++ volume13/aoj1305.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    while(cin >> n, n) {
+        map<string,vector<string> > v;
+        for(int i = 0; i < n; i++) {
+        }
+    }
+}
