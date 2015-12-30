38784f884c3075389be33a258581452567bc2123
wanimaru47 <wanimaru47@gmail.com>
2015-12-30 20:43:42 +0900

    aoj2620

--- volume26/aoj2620.cpp
+++ volume26/aoj2620.cpp
@@ -5,20 +5,21 @@ typedef pair<int,int> P;
 typedef pair<int,P> PP;
 typedef priority_queue<PP, vector<PP>, greater<PP>> QUE;
 
-int d[101][101][101][101];
+int d[501][501][4];
 
 int main() {
     int W, H, N;
     cin >> W >> H >> N;
 
     int x1, y1, x2, y2;
-    cin >> x1 >> y1 >> x2 >> y2;
+    cin >> y1 >> x1 >> y2 >> x2;
 
     map<char,int> dx, dy;
     dx['U'] = -1, dy['U'] = 0;
     dx['D'] = 1, dy['D'] = 0;
     dx['L'] = 0, dy['L'] = -1;
     dx['R'] = 0, dy['R'] = 1;
+    
 
     for(int i = 0; i < N; i++) {
         int sx, sy;
@@ -31,17 +32,17 @@ int main() {
                 int ny = sy + dy[code[j]];
                 if (!(0 <= nx && nx < H && 0 <= ny && ny < W)) continue;
                 if (code[j] == 'R') {
-                    d[nx][ny][nx + 1][ny] += 1;
-                    d[nx + 1][ny][nx][ny] += 1;
+                    d[nx][ny][1] += 1;
+                    d[nx + 1][ny][3] += 1;
                 } else if (code[j] == 'L') {
-                    d[sx][sy][sx + 1][sy] += 1;
-                    d[sx + 1][sy][sx][sy] += 1;
+                    d[sx][sy][1] += 1;
+                    d[sx + 1][sy][3] += 1;
                 } else if (code[j] == 'U') {
-                    d[sx][sy][sx][sy + 1] += 1;
-                    d[sx][sy + 1][sx][sy] += 1;
+                    d[sx][sy][0] += 1;
+                    d[sx][sy + 1][2] += 1;
                 } else {
-                    d[nx][ny][nx][ny + 1] += 1;
-                    d[nx][ny + 1][nx][ny] += 1;
+                    d[nx][ny][0] += 1;
+                    d[nx][ny + 1][2] += 1;
                 }
                 sx = nx;
                 sy = ny;
@@ -51,7 +52,7 @@ int main() {
 
     QUE que;
     que.push(PP(0, P(x1,y1)));
-    int D[101][101];
+    int D[501][501];
     int res = 1 << 28;
 
     for (int i = 0; i <= H; i++)
@@ -72,7 +73,7 @@ int main() {
         for (int i = 0; i < 4; i++) {
             int nx = x + ddx[i];
             int ny = y + ddy[i];
-            int cost_ = cost + d[x][y][nx][ny];
+            int cost_ = cost + d[x][y][i];
 
             if (0 <= nx && nx <= H && 0 <= ny && ny <= W && D[nx][ny] > cost_) {
                 D[nx][ny] = cost_;
