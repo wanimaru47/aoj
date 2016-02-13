8122d5fa2a8801715f6a04f858817b6eed275ead
wanimaru47 <wanimaru47@gmail.com>
2016-02-13 18:40:34 +0900

    write

---
+++ volume11/cpp/aoj1163.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vec;
+
+int main() {
+    int N, M;
+    while (cin >> N >> M, N || M) {
+        vec B(M), R(N);
+        for (auto &i : B) cin >> i;
+        for (auto &i : R) cin >> i;
+
+        
+    }
+
+    return 0;
+}
