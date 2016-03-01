0903e6c3af9f467968a7bd222edbb5584a930223
wanimaru47 <wanimaru47@gmail.com>
2016-03-01 11:27:22 +0900

    WA

--- volume11/cpp/aoj1162.cpp
+++ volume11/cpp/aoj1162.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef edge { int to, cap, cost, rev; };
+struct edge { int to, cap, cost, rev; };
 
 typedef vector<int> vec;
 typedef vector<vec> mat;
@@ -11,7 +11,7 @@ typedef vector<gvec> gmat;
 int N, M, C, S, T;
 
 int main() {
-    while (cin >> N >> M >> C >> A >> G, N || M || C || S || T) {
+    while (cin >> N >> M >> C >> S >> T, N || M || C || S || T) {
         gmat G(N + 1);
         vec dist(N + 1);
         vec prevv(N + 1);
@@ -20,6 +20,9 @@ int main() {
         vec xvec(M), yvec(M), dvec(M), cvec(M);
         for (int i = 0; i < M; i++) {
             cin >> xvec[i] >> yvec[i] >> dvec[i] >> cvec[i];
+            xvec[i]--;
+            yvec[i]--;
+            cvec[i]--;
         }
 
         mat q(C), r(C);
@@ -29,36 +32,36 @@ int main() {
             r[i].resize(p);
         }
 
-        mat c_cost(C);
+        mat c_cost(C, vec(222,0));
 
         for (int i = 0; i < C; i++) {
             for (auto &j : q[i]) cin >> j;
             for (auto &j : r[i]) cin >> j;
-            
-            c_cost[i].resize(222);
 
-            c_cost[i][0] = r[0];
+            c_cost[i][0] = r[i][0];
 
             int l = 0;
             for (int j = 0; j < 222; j++) {
                 if (l == q[i].size()) c_cost[i][j + 1] = c_cost[i][j];
                 else {
-                    if (q[i][l] > j) c_cost[i][j+1] = c_cost[i][j] + r[l];
+                    if (q[i][l] > j) c_cost[i][j+1] = c_cost[i][j] + r[i][l];
                     if (q[i][l] == j + 1) l++;
                 }
             }
         }
 
         auto add_edge = [&](int from, int to, int cap, int cost) {
-            G[from].push_back((edge){to, cap, cost, G[to].size()});
-            G[to].push_back((edge){from, 0, -cost, G[from].size() - 1});
-        }
+            G[from].push_back((edge){to, cap, cost, (int)(G[to].size())});
+            G[to].push_back((edge){from, 0, -cost, (int)(G[from].size()) - 1});
+        };
 
+        cout << "OK" << endl;
         for (int i = 0; i < M; i++) {
             add_edge(xvec[i], yvec[i], 1, c_cost[cvec[i]][dvec[i]]);
         }
+        cout << "KK" << endl;
 
-        auto mim_cost_flow = [&](int s, int t, int f) {
+        auto min_cost_flow = [&](int s, int t, int f) {
             int res = 0;
             const int INF = 1 <<  28;
 
@@ -69,7 +72,7 @@ int main() {
 
                 while (update) {
                     update = false;
-                    for (int v = 0;  v < V; v++) {
+                    for (int v = 0;  v < N; v++) {
                         if (dist[v] == INF) continue;
 
                         for (int i = 0; i < G[v].size(); i++) {
@@ -88,7 +91,7 @@ int main() {
 
                 int d = f;
                 for (int  v = t; v != s; v = prevv[v]) {
-                    d = min(d, G[prevv[v]][preve]].cap);
+                    d = min(d, G[prevv[v]][preve[v]].cap);
                 }
 
                 f -= d;
@@ -101,7 +104,7 @@ int main() {
             }
 
             return res;
-        }
+        };
 
         cout << min_cost_flow(S, T, 1) << endl;
     }
