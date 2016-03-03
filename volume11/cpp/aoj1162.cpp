daa13fb420b3241cbae6c6d6d26b05683af42391
wanimaru47 <wanimaru47@gmail.com>
2016-03-03 12:45:09 +0900

    core dumped

--- volume11/cpp/aoj1162.cpp
+++ volume11/cpp/aoj1162.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct edge { int to, cap, cost, rev; };
+struct edge { int to, cap, cost, rev, cmp; };
 
 typedef vector<int> vec;
 typedef vector<vec> mat;
@@ -9,6 +9,7 @@ typedef vector<edge> gvec;
 typedef vector<gvec> gmat;
 
 int N, M, C, S, T;
+const int INF = 1 <<  28;
 
 int main() {
     while (cin >> N >> M >> C >> S >> T, N || M || C || S || T) {
@@ -16,6 +17,7 @@ int main() {
         vec dist(N + 1);
         vec prevv(N + 1);
         vec preve(N + 1);
+        mat cmp_idx(C);
         S--;
         T--;
 
@@ -25,6 +27,7 @@ int main() {
             xvec[i]--;
             yvec[i]--;
             cvec[i]--;
+            cmp_idx[cvec[i]].push_back(i);
         }
 
         mat q(C), r(C);
@@ -52,28 +55,56 @@ int main() {
             }
         }
 
-        cout << "###########################################" << endl;
-        for (auto i : c_cost) {
-            for (auto j : i) cout << j << " ";
-            cout << endl;
-        }
-        cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << endl;
+        // cout << "###########################################" << endl;
+        // for (auto i : c_cost) {
+        //     for (auto j : i) cout << j << " ";
+        //     cout << endl;
+        // }
+        // cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << endl;
 
-        auto add_edge = [&](int from, int to, int cap, int cost) {
-            G[from].push_back((edge){to, cap, cost, (int)(G[to].size())});
-            G[to].push_back((edge){from, cap, cost, (int)(G[from].size()) - 1});
+        auto add_edge = [&](int from, int to, int cap, int cost, int cmp) {
+            G[from].push_back((edge){to, cap, cost, (int)(G[to].size()), cmp});
+            G[to].push_back((edge){from, cap, cost, (int)(G[from].size()) - 1, cmp});
         };
 
-        cout << "OK" << endl;
-        for (int i = 0; i < M; i++) {
-            add_edge(xvec[i], yvec[i], 1, c_cost[cvec[i]][dvec[i]]);
+        vector<mat> CC(C);
+
+        for (int i = 0; i < C; i++) {
+            CC[i].assign(N+1, vec(N+1, INF));
+
+            for (auto j : cmp_idx[i]) {
+                CC[i][xvec[j]][yvec[j]] = CC[i][yvec[j]][xvec[j]] = dvec[j];
+            }
+
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    for (int l = 0; l < N; l++) {
+                        CC[i][k][l] = min(CC[i][k][l], CC[i][k][j] + CC[i][j][l]);
+                    }
+                }
+            }
+
+            for (int j = 0; j < N; j++) {
+                for (int k = j; k < N; k++) {
+                    cout << j << " " << k << " ";
+                    if ((int)CC[i][j][k] < INF) {
+                        cout << "OK";
+                        add_edge(j, k, 1, c_cost[i][(int)CC[i][j][k]], i);
+                    }
+                    cout << endl;
+                }
+            }
         }
-        cout << "KK" << endl;
+
+
+        // cout << "OK" << endl;
+        // for (int i = 0; i < M; i++) {
+        //     add_edge(xvec[i], yvec[i], 1, c_cost[cvec[i]][dvec[i]]);
+        // }
+        // cout << "KK" << endl;
 
         auto min_cost_flow = [&](int s, int t, int f) {
-            // cout << "********** " << s << " " << t << endl;
             int res = 0;
-            const int INF = 1 <<  28;
 
             while (f > 0) {
                 dist.assign(N + 1, INF);
@@ -92,8 +123,7 @@ int main() {
                                 prevv[e.to] = v;
                                 preve[e.to] = i;
                                 update = true;
-
-                                cout << "Update: " << v << " to " << e.to << " , dist = " << dist[e.to] << endl;
+                                // cout << "Update: " << v << " to " << e.to << " , dist = " << dist[e.to] << endl;
                             }
                         }
                     }
@@ -114,7 +144,7 @@ int main() {
                     G[v][e.rev].cap += d;
                 }
 
-                cout << "flow: " << d << endl;
+                // cout << "flow: " << d << endl;
             }
 
             return res;
