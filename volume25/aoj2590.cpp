d740e11b3f9284365b7f87108699f585ea0f8628
wanimaru47 <wanimaru47@gmail.com>
2016-04-05 18:19:18 +0900

    fix indent

--- volume25/aoj2590.cpp
+++ volume25/aoj2590.cpp
@@ -4,10 +4,8 @@ using namespace std;
 int main() {
     int N, M, Q;
     while (cin >> N >> M >> Q, N || M || Q) {
-
         vector<string> S(2*Q+1), B(2*Q+1);
         vector<vector<int>> D(2*Q+1, vector<int>(M));
-
         string str;
 
         for (int i = 0; i < M; i++) {
@@ -17,11 +15,9 @@ int main() {
 
         for (int i = 1; i <= Q; i++) {
             cin >> S[i-1] >> B[i];
-
             for (int j = 0; j < N; j++) {
                 S[i+Q-1] += (S[i-1][j] == '0' ? '1' : '0');
             }
-
             for (int j = 0; j < M; j++) {
                 if (B[i-1][j] != B[i][j]) {
                     D[i-1][j] = 1;
@@ -39,14 +35,12 @@ int main() {
         } else {
             for (int i = 0; i < N; i++) {
                 auto tmp = str;
-
                 for (int j = 0; j < 2*Q; j++) {
                     if (S[j][i] == '0') continue;
                     for (int k = 0; k < M; k++) {
                         if (D[j][k] == 0) tmp[k] = '0';
                     }
                 }
-
                 for (int j = 0; j < M; j++) {
                     if (tmp[j] == '1') ans[j].insert(i);
                 }
