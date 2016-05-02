b13bf86f38d0baececc37c98f515d043222133c9
wanimaru47 <wanimaru47@gmail.com>
2016-05-03 02:06:36 +0900

    AC

--- volume11/cpp/aoj1190.cpp
+++ volume11/cpp/aoj1190.cpp
@@ -8,80 +8,89 @@ int N;
 vector<double> X, Y, L;
 
 bool check(int n, int m, int p, double h) {
-    double H = h * h;
-    double l1 = L[n] * L[n] - H;
-    double l2 = L[m] * L[m] - H;
-    double l3 = L[p] * L[p] - H;
+    P p1 = P(X[n], Y[n]);
+    P p2 = P(X[m], Y[m]);
+    P p3 = P(X[p], Y[p]);
+    double r1 = sqrt(L[n] * L[n] - h * h);
+    double r2 = sqrt(L[m] * L[m] - h * h);
+    double r3 = sqrt(L[p] * L[p] - h * h);
 
-    double s[] = {l1, l2, l3};
-    int v[] = {n, m, p};
-
-    for (int i = 0; i < 3; i++) {
-        for (int j = i + 1; j < 3; j++) {
-            int k = 3 - i - j;
-            P p1 = P(X[v[i]], Y[v[i]]);
-            P p2 = P(X[v[j]], Y[v[j]]);
-            P p3 = P(X[v[k]], Y[v[k]]);
-
-            auto dist0 = abs(p1 - p2);
-
-            if ((dist0 - sqrt(s[i]) - sqrt(s[j])) >= 0) return false;
-
-            if (dist0 <= sqrt(s[i]) + sqrt(s[j])) {
-                if (s[i] < s[j]){
-                    auto dist1 = abs(p1 - p3);
-                    if (dist1 <= (sqrt(s[k]) + sqrt(s[i]))) return true;
-                } else if (dist0 <= sqrt(s[j])) {
-                    auto dist1 = abs(p1 - p3);
-                    if (sqrt(dist1) <= (sqrt(s[k]) + sqrt(s[j]))) return true;
-                }
-            }
-
-            double C1 = (X[v[i]] - X[v[j]]) / (Y[v[j]] - Y[v[i]]);
-            double C2 = (s[i] - s[j] - (X[v[i]] * X[v[i]] - X[v[j]] * X[v[j]])
-                         - (Y[v[i]] * Y[v[i]] - Y[v[j]] * Y[v[j]])) / 2. / (2 * (Y[v[j]] - Y[v[i]]));
-            double C3 = 1. + C1 * C1;
-            double C4 = 2. * C1 * (C2 - Y[v[i]]) - 2 * X[v[i]];
-            double C5 = X[v[i]] * X[v[i]] + (C2 - Y[v[i]]) * (C2 - Y[v[i]]) - s[i];
-            double X1 = (- C4 + sqrt(C4 * C4 - 4 * C3 * C5)) / (2. * C3);
-            double X2 = (- C4 - sqrt(C4 * C4 - 4 * C3 * C5)) / (2. * C3);
-            double Y1 = C1 * X1 + C2;
-            double Y2 = C1 * X2 + C2;
-
-            if (abs(X1 - X2) < EPS && abs(Y1 - Y2) < EPS) {
-                double dist1 = (X1 - X[v[k]]) * (X1 - X[v[k]]) + (Y1 - Y[v[k]]) * (Y1 - Y[v[k]]);
-                if (abs(dist1 - s[k]) < EPS) return true;
-            }
-
-            double dist1 = (X1 - X[v[k]]) * (X1 - X[v[k]]) + (Y1 - Y[v[k]]) * (Y1 - Y[v[k]]);
-            double dist2 = (X2 - X[v[k]]) * (X2 - X[v[k]]) + (Y2 - Y[v[k]]) * (Y2 - Y[v[k]]);
-
-            if (dist1 <= s[k] || dist2 <= s[k]) return true;
+    if (abs(p1 - p2) > r1 + r2) return false;
+    if (abs(p1 - p2) <= abs(r2 - r1)) {
+        if (r1 <= r2 && abs(p1 - p3) <= r1 + r3) {
+            return true;
         }
+        else if (r2 <= r1 && abs(p2 - p3) <= r2 + r3) {
+            return true;
+        }
+        else return false;
+    }
+
+    if (abs(p1 - p2) == r1 + r2) {
+        P p = p1 + (r1 / (r1 + r2)) * (p2 - p1);
+        if (abs(p - p3) <= r3) return true;
+        return false;
+    } else if (abs(r1 - r2) < abs(p1 - p2) && abs(p1 - p2) < r1 + r2) {
+        double A = 2. * (p2.real() - p1.real());
+        double B = 2. * (p2.imag() - p1.imag());
+        double C = r1 * r1 - r2 * r2 - (p1.real() * p1.real() - p2.real() * p2.real()) - (p1.imag() * p1.imag() - p2.imag() * p2.imag());
+        double X1, X2, Y1, Y2;
+        double Ad, Bd, Cd;
+        if (B == 0) {
+            X1 = X2 = C / A;
+            Y1 = p1.imag() - sqrt(r1 * r1 - (X1 - p1.real()) * (X1 - p1.real()));
+            Y2 = p1.imag() + sqrt(r1 * r1 - (X1 - p1.real()) * (X1 - p1.real()));
+        } else if (A == 0) {
+            Y1 = Y2 = C / B;
+            X1 = p1.real() - sqrt(r1 * r1 - (Y1 - p1.imag()) * (Y1 - p1.imag()));
+            X2 = p1.real() + sqrt(r1 * r1 - (Y1 - p1.imag()) * (Y1 - p1.imag()));
+        } else {
+            Ad = 1. + (A / B) * (A / B);
+            Bd = -2 * p1.real() - 2 * (A / B) * (C / B - p1.imag());
+            Cd = (C / B - p1.imag()) * (C / B - p1.imag()) - r1 * r1 + p1.real() * p1.real();
+            X1 = (- Bd + sqrt(Bd * Bd - 4. * Ad * Cd)) / (2. * Ad);
+            X2 = (- Bd - sqrt(Bd * Bd - 4. * Ad * Cd)) / (2. * Ad);
+            Y1 = (- A / B) * X1 + (C / B);
+            Y2 = (- A / B) * X2 + (C / B);
+        }
+        P p1d = P(X1, Y1);
+        P p2d = P(X2, Y2);
+
+        if (abs(p1d - p3) <= r3) return true;
+        if (abs(p2d - p3) <= r3) return true;
     }
 
     return false;
 }
 
 void solve() {
-    double res = 1LL << 28;
+    double res_r = 1LL << 28;
+    double res_l = 1LL << 28;
     for (int i = 0; i < N; i++) {
         for (int j = i; j < N; j++) {
             for (int k = j; k < N; k++) {
-                double l = 1., r = res;
+                int v[] = {i, j, k};
+                double l = 0., r = min(L[i], min(L[j], L[k]));
                 for (int loop = 0; loop < 100; loop++) {
-                    if (check(i, j, k, (l + r) / 2.)) {
+                    bool flag = false;
+                    for (int I = 0; I < 3; I++) {
+                        for (int J = I + 1; J < 3; J++) {
+                            int K = 3 - J - I;
+                            flag |= check(v[I], v[J], v[K], (l + r) / 2.);
+                        }
+                    }
+                    if (flag) {
                         l = (l + r) / 2.;
                     } else {
                         r = (l + r) / 2.;
                     }
+                    res_r = min(res_r, r);
+                    res_l = min(res_l, l);
                 }
-                res = min(res, l);
             }
         }
     }
-
-    printf("%.10f\n", res);
+    printf("%.10f\n", res_r);
 }
 
 int main() {
@@ -91,16 +100,7 @@ int main() {
             cin >> X[i] >> Y[i] >> L[i];
         }
 
-        if (N == 1) cout << L[0] << endl;
-        else {
-            if (N == 2) {
-                N++;
-                X.push_back(X[0]);
-                Y.push_back(Y[0]);
-                L.push_back(L[0]);
-            }
-            solve();
-        }
+        solve();
     }
 
     return 0;
