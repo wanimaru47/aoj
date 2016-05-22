d578908f852b8b6fb02f9ce0e88da3c0d5e57780
wanimaru47 <wanimaru47@gmail.com>
2016-05-22 15:44:01 +0900

    AC

--- volume20/cpp/aoj2002.cpp
+++ volume20/cpp/aoj2002.cpp
@@ -2,89 +2,91 @@
 using namespace std;
 
 typedef pair<int,int> P;
-
 int N, M;
 
-P check(vector<vector<char> > &v) {
-    for (int i = 0; i < v.size(); i++) {
-        for (int j = 0; j < v[i].size(); j++) {
-            if (v[i][j] != '.') return P(i, j);
+bool check(vector<vector<char> > &v) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (v[i][j] != '.' && v[i][j] != 'x') return true;
         }
     }
 
-    return P(-1, -1);
+    return false;
 }
 
-int main ()
-{
-    int X; cin >> X;
-    while (cin >> N >> M, X--) {
-        vector<vector<char> > v(N, vector<char>(M));
-        for (auto &i : v) for (auto &j : i) cin >> j;
+bool check2(vector<vector<char> > &v, int sx, int sy, int gx, int gy, char str) {
+    set<char> res;
+    for (int i = sx; i <= gx; i++) {
+        for (int j = sy; j <= gy; j++) {
+            if (v[i][j] == 'x' || v[i][j] == str);
+            else return false;
+        }
+    }
 
-        P start;
-        bool res = true;
-        while ((start = check(v)) != P(-1,-1)) {
-            char S = v[start.first][start.second];
-            vector<P> chain;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (v[i][j] == S)chain.push_back(P(i,j));
-                }
-            }
-            sort(chain.begin(), chain.end());
-            int sx = chain[0].first;
-            int sy = chain[0].second;
-            int gx = chain[chain.size()-1].first;
-            int gy = chain[chain.size()-1].second;
-            int num = (gx - sx + 1) * (gy - sy + 1);
+    return true;
+}
 
-            cout << "OK" << endl;
-            if (num == chain.size()) {
-                for (int i = sx; i <= gx; i++) {
-                    for (int j = sy; j <= gy; j++) {
-                        if (S != v[i][j]) res = false;
-                    }
-                }
-            } else {
-                // if (!(sx == chain[1].first && sy + 1 == chain[1].second) && !(sy == chain[1].second && sx == chain[1].first + 1)) res = false;
-                if (!(chain[1].first <= sx && sx <= gx && sy + 1 == chain[1].second) && !(chain[1].second <= sy && sy <= gy && sx + 1 == chain[1].first)) res = false;
-                sx = chain[1].first;
-                sy = chain[1].second;
-                num = (gx - sx + 1) * (gy - sy + 1);
-                if (num == chain.size() - 1) {
-                    for (int i = sx; i <= gx; i++) {
-                        for (int j = sy; j <= gy; j++) {
-                            if (S != v[i][j]) res = false;
-                        }
-                    }
-                } else {
-                    sx = chain[0].first;
-                    sy = chain[0].second;
-                    gx = chain[chain.size()-2].first;
-                    gy = chain[chain.size()-2].second;
-                    if (!(sy <= chain[chain.size() - 1].second && chain[chain.size() - 1].second <= gy && gx + 1 == chain[chain.size() - 1].first)
-                        && !(sx <= chain[chain.size()-1].first && chain[chain.size() - 1].first <= gx && gy + 1 == chain[chain.size() - 1].second)) res = false;
-                    num = (gx - sx + 1) * (gy - sy + 1);
-                    if (num == chain.size() - 1) {
-                        for (int i = sx; i <= gx; i++) {
-                            for (int j = sy; j <= gy; j++) {
-                                if (S != v[i][j]) res = false;
-                            }
-                        }
-                    }
-                }
-            }
-            if (res == false) break;
-            else {
-                for (int i = 0; i < N; i++) {
-                    for (int j = 0; j < M; j++) {
-                        if (S == v[i][j]) v[i][j] = '.';
-                    }
-                }
+vector<int> found(vector<vector<char> > &v, char str) {
+    int sx = 1 << 28, sy = 1 << 28, gx = -1, gy = -1;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (v[i][j] == str) {
+                sx = min(sx, i);
+                gx = max(gx, i);
+                sy = min(sy, j);
+                gy = max(gy, j);
             }
         }
-        cout << (res ? "YES" : "NO") << endl;
+    }
+    
+    if (check2(v, sx, sy, gx, gy, str)) {
+        return vector<int>{sx, sy, gx, gy};
+    }
+
+    return vector<int>{-1, -1, -1, -1};
+}
+
+void solve() {
+    cin >> N >> M;
+    vector<vector<char> > v(N, vector<char>(M));
+    set<int> s;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> v[i][j];
+            if (v[i][j] != '.') s.insert(v[i][j]);
+        }
+    }
+
+    bool res = true;
+
+    while (check(v)) {
+        bool flag = false;
+        auto tmp = s;
+        for (auto str : tmp) {
+            auto L = found(v, str);
+
+            if (L[0] < 0) {
+                continue;
+            }
+            s.erase(str);
+            flag = true;
+            for (int i = L[0]; i <= L[2]; i++)
+                for (int j = L[1]; j <= L[3]; j++)
+                    v[i][j] = 'x';
+        }
+        if (!flag) {
+            res = false;
+            break;
+        }
+    }
+
+    cout << (res ? "SAFE" : "SUSPICIOUS") << endl;
+}
+
+int main() {
+    int Q; cin >> Q;
+    for (int q = 0; q < Q; q++) {
+        solve();
     }
 
     return 0;
