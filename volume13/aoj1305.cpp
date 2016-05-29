4e2610f0324cedbadda446ca8c445a82a50fe061
wanimaru47 <wanimaru47@gmail.com>
2016-05-29 19:28:06 +0900

    save

--- volume13/aoj1305.cpp
+++ volume13/aoj1305.cpp
@@ -1,11 +1,53 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n;
-    while(cin >> n, n) {
-        map<string,vector<string> > v;
-        for(int i = 0; i < n; i++) {
+int N;
+
+string Input(map<string, set<string> >& m) {
+    string str;
+    int count = 0;
+    stringstream ss;
+    cin >> str;
+    for (int i = 0; i < (int)str.size(); i++) {
+        if (str[i] == ':' || str[i] == ',' || str[i] == '.') {
+            count++;
+            str[i] = ' ';
         }
     }
+
+    ss << str;
+    string top;
+    ss >> top;
+    for (int i = 0; i < count - 1; i++) {
+        string s;
+        ss >> s;
+        m[top].insert(s);
+    }
+
+    return top;
+}
+
+void solve() {
+    map<string, set<string> > m;
+    vector<string> sSet(N);
+    for (int i = 0; i < N; i++) sSet[i] = Input(m);
+
+    set<string> ans;
+    queue<string> que;
+    for (auto i : m[sSet[0]]) que.push(i);
+    while (que.size()) {
+        string str = que.front(); que.pop();
+        if (m.find(str) == m.end()) ans.insert(str);
+        else {
+            for (auto i : m[str]) que.push(i);
+        }
+    }
+
+    cout << ans.size() << endl;
+}
+
+int main() {
+    while (cin >> N, N) solve();
+
+    return 0;
 }
