2d60d147a4ddf077227bc1670f59a53247337b43
wanimaru47 <wanimaru47@gmail.com>
2016-06-02 00:28:17 +0900

    WA

--- volume24/aoj2401.cpp
+++ volume24/aoj2401.cpp
@@ -4,15 +4,16 @@ using namespace std;
 vector<string> opr{"->", "+", "*", "-"};
 vector<string> num{"1", "2", "3", "4"};
 
-void add_para(string exp) {
+void add_para(string &exp) {
+    // cout << ">> " << exp << endl;
     auto find_idx = exp.find("4");
     while (find_idx != string::npos) {
-        if (exp[find_idx + 1] == "(") {
+        if (exp[find_idx + 1] == '(') {
             int end_idx = find_idx + 2;
             int count = 1;
             while (count) {
                 if (exp[end_idx] == '(') count++;
-                else if (exp[end_idx] == ')') count++;
+                else if (exp[end_idx] == ')') count--;
                 end_idx++;
             }
             exp.insert(end_idx, ")");
@@ -26,9 +27,12 @@ void add_para(string exp) {
 
         find_idx = exp.find("4");
     }
+
+    // cout << "<< " << exp << endl;
 }
 
 bool calc(string exp, int bits) {
+    // cout << exp << endl;
     if (exp == "T") return true;
     else if (exp == "F") return false;
     else if (exp.size() == 1) {
@@ -36,19 +40,29 @@ bool calc(string exp, int bits) {
         else return false;
     }
 
-    if (exp[1] == '5') return !calc(string(exp.begin() + 2, exp.end() - 1));
+    if (exp[1] == '5') return !calc(string(exp.begin() + 2, exp.end() - 1), bits);
     int idx;
-    for (int i = 0, len = exp.size(), count = 0; i < len; i++) {
+    for (int i = 1, len = exp.size(), count = 0; i < len - 1; i++) {
         if (exp[i] == '(') count++;
         else if (exp[i] == ')') count--;
+        if (count == 0 && '1' <= exp[i] && exp[i] <= '5') idx = i;
     }
+
+    // cout << "idx=" << idx << endl;
+
+    bool lhs = calc(string(exp.begin() + 1, exp.begin() + idx), bits);
+    bool rhs = calc(string(exp.begin() + idx + 1, exp.end() - 1), bits);
+
+    if (exp[idx] == '1') return (lhs && !rhs);
+    if (exp[idx] == '2') return (lhs || rhs);
+    if (exp[idx] == '3') return (lhs && rhs);
 }
 
 void solve(string str) {
     for (int i = 0, len = opr.size(); i < len; i++) {
         auto find_idx = str.find(opr[i]);
         while (find_idx != string::npos) {
-            str.repalce(find_idx, (int)opr[i].size(), num[i]);
+            str.replace(find_idx, (int)(opr[i].size()), num[i]);
             find_idx = str.find(opr[i]);
         }
     }
@@ -56,8 +70,11 @@ void solve(string str) {
     string exp1 = string(str.begin(), str.begin() + eq_idx);
     string exp2 = string(str.begin() + eq_idx + 1, str.end());
 
+    add_para(exp1);
+    add_para(exp2);
+
+    bool flag = true;
     for (int i = 0; i < (1 << 11); i++) {
-        bool flag = true;
         if (calc(exp1, i) != calc(exp2, i)) {
             flag = false;
             break;
