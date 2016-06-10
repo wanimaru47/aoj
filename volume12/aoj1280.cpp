2c9e18d01755f514220131406744eda7a0b68f01
wanimaru47 <wanimaru47@gmail.com>
2016-06-10 15:18:10 +0900

    AC

--- volume12/aoj1280.cpp
+++ volume12/aoj1280.cpp
@@ -8,46 +8,68 @@ typedef vector<vec> mat;
 const int INF = 1 << 28;
 int N, M;
 
-bool check(vec& v, vec& t) {
-    for (int i = 0; i < N; i++) {
-        if (v[i] >= t[i]) return false;
-    }
-
-    return true;
-}
-
 void solve() {
-    mat dp(10001, vec(N, 0));
-    vec count(10001);
+    vector<vector<P> > v(N);
     for (int i = 0; i < M; i++) {
-        int x, y, w;
-        cin >> x >> y >> w;
-        dp[w][x-1]++;
-        dp[w][y-1]++;
-        count[w]++;
+        int x, y, z;
+        cin >> x >> y >> z;
+        x--;
+        y--;
+        v[x].push_back(P(z, y));
+        v[y].push_back(P(z, x));
     }
 
+    for (auto& i : v) {
+        sort(i.begin(), i.end());
+    }
+
+    int res = INF;
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < 10000; j++) {
-            dp[j+1][i] += dp[j][i];
+        for (int j = 0; j < v[i].size(); j++) {
+            priority_queue<P, vector<P>, greater<P> > que;
+            vec used(N, false);
+            used[i] = used[v[i][j].second] = true;
+            for (auto k : v[i]) {
+                if (k.first >= v[i][j].first) que.push(k);
+            }
+
+            for (auto k : v[v[i][j].second]) {
+                if (k.first >= v[i][j].first) que.push(k);
+            }
+
+            int Max = v[i][j].first;
+
+            while (que.size()) {
+                P p = que.top(); que.pop();
+                int w = p.first;
+                int now = p.second;
+                if (used[now]) continue;
+                used[now] = true;
+                Max = max(Max, w);
+                if (Max - v[i][j].first > res) break;
+                for (auto e : v[now]) {
+                    int tmp = e.first - v[i][j].first;
+                    if (tmp >= 0 && tmp < res && !used[e.second]) {
+                        que.push(e);
+                    }
+                }
+            }
+
+            bool flag = true;
+            for (int i = 0; i < N; i++) {
+                if (!used[i]) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                res = min(res, Max - v[i][j].first);
+            }
         }
     }
 
-    for (int i = 0; i < 10000; i++) {
-        count[i+1] += count[i];
-    }
-
-    int i = 0;
-    int ans = INF;
-    for (int j = 0; j <= 10000; j++) {
-        if (!check(dp[i], dp[j])) continue;
-        while (check(dp[i], dp[j])) {
-            if (count[j] - count[i] >= N - 1) ans = min(ans, j - i - 1);
-            i++;
-        }
-    }
-
-    cout << (ans >= 10000 ? -1 : ans) << endl;
+    cout << (res > 10000 ? -1 : res) << endl;
 }
 
 int main() {
