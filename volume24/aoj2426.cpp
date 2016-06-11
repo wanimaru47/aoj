03d8c899bb1a323f645ee035218a65281fa3cab2
wanimaru47 <wanimaru47@gmail.com>
2016-06-11 22:06:28 +0900

    AC

--- volume24/aoj2426.cpp
+++ volume24/aoj2426.cpp
@@ -15,6 +15,7 @@ int main() {
     }
 
     sort(v.begin(), v.end());
+
     int len = sqrt(N);
     vector<vector<P> > m((N + len - 1) / len), n((N + len - 1) / len);
 
@@ -36,14 +37,14 @@ int main() {
             int n_first = n[i][0].first;
             int n_last = n[i][n[i].size()-1].first;
             if ((sx <= n_first && n_first <= gx) && (sx <= n_last && n_last <= gx)) {
-                int low = lower_bound(m[i].begin(), m[i].end(), P(sy, -INF)) - m[i].begin();
-                int up = upper_bound(m[i].begin(), m[i].end(), P(gy, INF)) - m[i].begin();
+                int low = lower_bound(m[i].begin(), m[i].end(), P(sy, sx)) - m[i].begin();
+                int up = upper_bound(m[i].begin(), m[i].end(), P(gy, gx)) - m[i].begin();
                 ans += up - low;
             } else {
                 if ((n_first <= sx && sx <= n_last) || (n_first <= gx && gx <= n_last)) {
                     int low = lower_bound(n[i].begin(), n[i].end(), P(sx,sy)) - n[i].begin();
                     int up = upper_bound(n[i].begin(), n[i].end(), P(gx,gy)) - n[i].begin();
-                    for (low; low <= up; low++) {
+                    for (low; low <= up && low < n[i].size(); low++) {
                         if (
                             sx <= n[i][low].first && n[i][low].first <= gx
                             && sy <= n[i][low].second && n[i][low].second <= gy
