69c0e7f5756c49f76825471a5ba2d1e603d84d35
wanimaru47 <wanimaru47@gmail.com>
2016-06-13 15:02:35 +0900

    AC

--- volume26/aoj2640.cpp
+++ volume26/aoj2640.cpp
@@ -1,105 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef pair<int,int> P;
-typedef pair<int,P> PP;
-typedef pair<PP,P> PPP;
-typedef priority_queue<PPP, vector<PPP>, greater<PPP>> PQ;
+int H, W;
+const string DIR = "^>v<";
+const int dx[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
 
-string str = "^>v<";
-const int dx[] = {-1, 0, 1, 0};
-const int dy[] = {0, 1, 0, -1};
-
-const int dx_[] = {-1, -1, 0, 1, 1, 1, 0, -1};
-const int dy_[] = {0, 1, 1, 1, 0, -1, -1, -1};
-
-int main () {
-    int N, M, sx, sy, sdir;
-    cin >> N >> M;
-    vector<string> v(N);
-    for (int i = 0; i < N; i++) {
-        cin >> v[i];
-        for (int j = 0; j < M; j++) {
-            auto flag = find(str.begin(), str.end(), v[i][j]) - str.begin();
-            if (flag < str.size()) {
-                sx = i;
-                sy = j;
-                sdir = flag;
-            }
+int main() {
+    cin >> H >> W;
+    vector<vector<char> > V(H + 2, vector<char>(W + 2, '#'));
+    for (int i = 1; i <= H; i++) {
+        for (int j = 1; j <= W; j++) {
+            cin >> V[i][j];
         }
     }
 
-    PQ que;
-    int d[N][M][4][4];
-    for (int i = 0; i < N; i++) 
-        for (int j = 0; j < M; j++)
-            for (int k = 0; k < 4; k++) 
-                for (int l = 0; l < 4; l++) d[i][j][k][l] = 1 << 28;
+    int sx, sy, gx, gy, dir;
+    for (int i = 1; i <= H; i++) {
+        for (int j = 1; j <= W; j++) {
+            if (V[i][j] == 'G') gx = i, gy = j;
+            else if (DIR.find(V[i][j]) != string::npos)
+                sx = i, sy = j, dir = DIR.find(V[i][j]);
+        }
+    }
 
-    for (int i = 0; i < 4; i++) {
-        int nx, ny;
-        nx = sx + dx_[(2 * sdir + i) % 8];
-        ny = sy + dy_[(2 * sdir + i) % 8];
-        if (0 <= nx && nx < v.size() && 0 <= ny && ny < v[nx].size()) {
-            if (v[nx][ny] == '#') {
-                que.push(PPP(PP(0, P(i, sdir)), P(sx, sy)));
-                d[sx][sy][sdir][i] = 0;
-            }
+    vector<vector<vector<bool> > > used(H+2, vector<vector<bool> >(W+2, vector<bool>(4, false)));
+    while (!used[sx][sy][dir] && V[sx][sy] != 'G') {
+        used[sx][sy][dir] = true;
+        int nx = sx + dx[dir * 2];
+        int ny = sy + dy[dir * 2];
+        int nhx = nx + dx[(dir * 2 + 2) % 8];
+        int nhy = ny + dy[(dir * 2 + 2) % 8];
+        if (V[nx][ny] == '#') {
+            dir = (dir + 3) % 4;
         } else {
-            que.push(PPP(PP(0, P(i, sdir)), P(sx, sy)));
-            d[sx][sy][sdir][i] = 0;
-        }
-    }
-    
-    int res = 1 << 28;
-    while (que.size()) {
-        PPP pp = que.top(); que.pop();
-        int x = pp.second.first;
-        int y = pp.second.second;
-        int dir = pp.first.second.second;
-        int c = pp.first.second.first;
-        int cost = pp.first.first;
+            sx = nx;
+            sy = ny;
 
-        if (d[x][y][dir][c] < cost) continue;
-        if (v[x][y] == 'G') res = min(res, cost);
-
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
-
-        if (0 <= nx && nx < N && 0 <= ny && ny < M && v[nx][ny] != '#') {
-            if ((c == 1 || c == 2) && d[nx][ny][dir][c + 1] > cost + 1) {
-                d[nx][ny][dir][c + 1] = cost + 1;
-                que.push(PPP(PP(cost + 1, P(c + 1, dir)), P(nx, ny)));
-            }
-        }
-        if ((c == 2 || c == 3) && d[x][y][(dir + 1) % 4][c - 2] > cost + 1) {
-            d[x][y][(dir + 1) % 4][c - 2] = cost + 1;
-            que.push(PPP(PP(cost + 1, P(c - 2, (dir + 1) % 4)), P(x, y)));
-        }
-        if ((c == 0 || c == 1) && d[x][y][(dir + 3) % 4][c + 2] > cost + 1) {
-            d[x][y][(dir + 3) % 4][c + 2] = cost + 1;
-            que.push(PPP(PP(cost + 1, P(c + 2, (dir + 3) % 4)), P(x, y)));
-        }
-
-        for (int i = 0; i < 4; i++) {
-            if (c == i) continue;
-            int nx = x + dx_[(2 * dir + i) % 8];
-            int ny = y + dy_[(2 * dir + i) % 8];
-            if (0 <= nx && nx < N && 0 <= ny && ny < M) {
-                if (v[nx][ny] == '#' && d[x][y][dir][i] > cost + 1) {
-                    d[x][y][dir][i] = cost + 1;
-                    que.push(PPP(PP(cost + 1, P(i, dir)), P(x, y)));
-                }
-            } else {
-                if (d[x][y][dir][i] > cost + 1) {
-                    d[x][y][dir][i] = cost + 1;
-                    que.push(PPP(PP(cost + 1, P(i, dir)), P(x, y)));
-                }
+            if (V[nhx][nhy] != '#') {
+                dir = (dir + 1) % 4;
             }
         }
     }
 
-    cout << (res < (1 << 28) ? res : -1) << endl;
-    
+    if (V[sx][sy] == 'G') {
+        int ans = 1;
+        for (int i = 1; i <= H; i++) {
+            for (int j = 1; j <= W; j++) {
+                bool flag = false;
+                for (int k = 0; k < 4; k++) {
+                    if (used[i][j][k]) flag = true;
+                }
+                if (flag) ans++;
+            }
+        }
+        cout << ans << endl;
+    } else {
+        cout << -1 << endl;
+    }
+
     return 0;
 }
