e9e3c6c33f06f9989b1ae9bfaf1969cd23dff728
wanimaru47 <wanimaru47@gmail.com>
2016-07-10 21:07:05 +0900

    AC

---
+++ volume05/cpp/aoj0554.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int sum = 0;
+    for (int i = 0; i < 4; i++) {
+        int t; cin >> t;
+        sum += t;
+    }
+
+    cout << sum / 60 << endl;
+    cout << sum % 60 << endl;
+
+    return 0;
+}
