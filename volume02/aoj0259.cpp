238912b924ba391a845d267d0bcfe6c1da6e9ed4
wanimaru47 <wanimaru47@gmail.com>
2016-08-15 22:49:35 +0900

    AC

---
+++ volume02/aoj0259.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    string str;
+    while (cin >> str) {
+        bool f = false;
+        for (auto i : str) if (i != '0') f = true;
+
+        if (!f) break;
+
+        f = false;
+        for (auto i : str) if (i != str[0]) f = true;
+
+        if (!f) {
+            cout << "NA" << endl;
+            continue;
+        }
+
+        int count = 0;
+        int now;
+        stringstream org;
+        org << str;
+        org >> now;
+        while (1) {
+            if (now == 6174) {
+                cout << count << endl;
+                break;
+            }
+            count++;
+            sort(str.begin(), str.end());
+            auto rstr = str;
+            reverse(rstr.begin(), rstr.end());
+
+            // for (auto i : str) cout << i; cout << endl;
+            // for (auto i : rstr) cout << i; cout << endl;
+
+            stringstream ss;
+            ss << rstr << " " << str;
+            int A, B;
+            ss >> A >> B;
+            now = A - B;
+            stringstream next;
+            next << now;
+            next >> str;
+            while (str.size() < 4) str = "0" + str;
+        }
+    }
+
+    return 0;
+}
