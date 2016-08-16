84293b48071d4b3b8c40edff53ba8d0ebfcb09bc
wanimaru47 <wanimaru47@gmail.com>
2016-08-16 20:20:41 +0900

    AC

--- volume01/cpp/aoj0130.cpp
+++ volume01/cpp/aoj0130.cpp
@@ -1,23 +1,25 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main ()
-{
-    int n;
-    cin >> n;
-
-    while (n--) {
+int main() {
+    int N;
+    cin >> N;
+    while (N--) {
         string str;
+        cin >> str;
+        string ans;
+        ans += str[0];
+        int corsor = 0;
+        for (int i = 1; i * 3 + 1 <= str.size(); i++) {
+            auto pre = string(str.begin() + (i-1)*3+1, str.begin() + (i-1)*3+3);
+            if (pre == "<-") corsor--;
+            else corsor++;
 
-        vector<char> res;
-        res.push_back(str[0]);
+            if (corsor < 0) corsor = 0, ans = str[i*3] + ans;
+            if (corsor >= ans.size()) ans += str[i*3];
 
-        for (int i = 3; i < str.size(); i++) {
-            auto ite = find()
         }
+        cout << ans << endl;
     }
 
     return 0;
