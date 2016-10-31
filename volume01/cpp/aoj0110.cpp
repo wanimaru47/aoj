616cd3b08bf2c2cf15874b3ba71956efd9f5ab68
wanimaru47 <wanimaru47@gmail.com>
2016-10-31 21:36:52 +0900

    AC 0110

--- volume01/cpp/aoj0110.cpp
+++ volume01/cpp/aoj0110.cpp
@@ -1,60 +1,54 @@
-#include <iostream>
-#include <cstring>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool cal (string n1, string n2, string n3) {
-    cout << n1 << " " << n2 << " " << n3 << endl;
-    bool res = false;
-    string sum, num;
-    if (n1.size() > n2.size()) {sum = n1; num = n2;}
-    else {sum = n2; num = n1;}
-    char t = '0';
-    for (int i = 0; i < num.size() + 1; i++) { 
-        int tmp = 0;
-        int t1 = sum[sum.size() - 1 - i] - '0';
-        int t2 = t - '0';
-        if (i < num.size()) {
-            int t3 = num[num.size() - 1 - i] - '0';
-            tmp += t3;
-        }
-        tmp += t1 + t2;
-        if (tmp >= 10) t = '1';
+string sub(string s1, string s2) {
+    int l1 = s1.size();
+    int l2 = s2.size();
+    string res;
+    for (int i = 0, c = 0; c || i < max(l1, l2); i++) {
+        int tmp = c;
+        if (l1 - 1 - i > -1) tmp += (s1[l1 - 1 - i] - '0');
+        if (l2 - 1 - i > -1) tmp += (s2[l2 - 1 - i] - '0');
+
+        c = tmp / 10;
+
         tmp %= 10;
-        sum[sum.size() - 1 - i] = (char)tmp;
+
+        res = (char)('0' + tmp) + res;
     }
-    cout << sum << endl;
-    if (sum == n3) res = true;
-    
+
     return res;
 }
 
-int main ()
-{
-    char str[127];
+int main() {
+    string str;
     while (cin >> str) {
         int ans = -1;
         for (int i = 0; i < 10; i++) {
-            auto tmp = str;
-            int k;
-            for (k = 0; str[k] != '\0'; k++);
-            for (int j = 0; j < k; j++)
-                if (tmp[j] == 'X') tmp[j] = (char)i;
-            cout << tmp << endl;
-            char *tp;
-            k = 0;
-            string num[3];
-            tp = strtok(tmp, "+=");
-            while (tp != NULL) {
-                num[k] = (string)tp;
-                tp = strtok(NULL, "+=");
-                k++;
+            string tmp = str;
+            for (auto& j : tmp) if (j == 'X') j = '0' + i;
+            bool flag = true;
+            // for (int j = 0; j < tmp.size(); j++) {
+            //     if (j == 0 && tmp[0] == '0') flag = false;
+            //     if (j > 0 && tmp[j-1] == '+' && tmp[j] == '0') flag = false;
+            //     if (j > 0 && tmp[j-1] == '=' && tmp[j] == '0') flag = false;
+            // }
+            if (!flag) continue;
+            string a, b, c;
+            a = string(tmp.begin(), tmp.begin() + tmp.find('+'));
+            b = string(tmp.begin() + tmp.find('+') + 1, tmp.begin() + tmp.find('='));
+            c = string(tmp.begin() + tmp.find('=') + 1, tmp.end());
+
+            string p = sub(a, b);
+
+            if (c == p) {
+                ans = i;
+                break;
             }
-            cout << num[0] << " " << num[1] << " " << num[2] << endl;
-            if (cal(num[0], num[1], num[2])) ans = i;
         }
         if (ans < 0) cout << "NA" << endl;
         else cout << ans << endl;
-     }
+    }
 
     return 0;
 }
