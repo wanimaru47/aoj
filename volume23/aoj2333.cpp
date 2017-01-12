304ea9ac4130e61d3ebf2a33e6cb65007185b6e2
wanimaru47 <wanimaru47@gmail.com>
2017-01-12 11:35:32 +0900

    AC 2333

--- volume23/aoj2333.cpp
+++ volume23/aoj2333.cpp
@@ -7,43 +7,41 @@ int main() {
     int n, m;
     const ll MOD = 1e9+7;
     cin >> n >> m;
-    vector<int> v(n);
-    for (auto& i : v) cin >> i;
+    vector<int> v(n+1);
+    for (int i = 1; i <= n; i++) cin >> v[i];
+    v.push_back(1<<26);
     sort(v.begin(), v.end());
+    auto sum = v;
+    for (int i = 0; i < n; i++)
+        sum[i] += sum[i-1];
 
-    vector<ll> dp(m+1);
-    vector<int> used(m+1);
-    vector<int> t(m+1, 1000000000);
-    dp[0] = 1LL;
-    for (int i = 0; i < n; i++) {
-        for (int j = m, s = 1000000000; j - v[i] >= 0; j--) {
-            if (dp[j-v[i]]) {
-                dp[j] += dp[j-v[i]];
-                dp[j] %= MOD;
-                used[j-v[i]] = true;
-                if (i+1<n) {
-                    t[j] = s = min(s, min(t[j-v[i]], v[i+1]));
-                } else {
-                    t[j] = min(t[j], s);
+    ll ans = 0;
+    for (int i = 1; i <= n+1; i++) {
+        if (sum[i-1] > m) break;
+
+        vector<ll> dp(m+1);
+        vector<int> used(m+1);
+        dp[sum[i-1]] = 1LL;
+
+        for (int j = i + 1; j <= n; j++) {
+            for (int k = m; k - v[j] >= sum[i-1]; k--) {
+                if (dp[k - v[j]]) {
+                    dp[k] += dp[k - v[j]];
+                    dp[k] %= MOD;
+                    used[k - v[j]] = true;
                 }
             }
         }
-        for (auto j : dp) cout << j << " "; cout << endl;
-    }
 
-    ll ans = 0;
-    for (int i = 0; i <= m; i++) {
-        if (!used[i] && i + t[i] > m) {
-            if (t[i] == 1000000000) {
-                if (i + v[0] > m)
-                    ans += dp[i];
-            } else {
-                ans += dp[i];
+        for (int j = 1; j <= m; j++) {
+            if (!used[j] && j + v[i] > m) {
+                ans += dp[j];
+                ans %= MOD;
             }
         }
     }
 
-    cout << ans << endl;
+    cout << max(ans,1LL) << endl;
 
     return 0;
 }
